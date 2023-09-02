58236ebadb4828e92f7b8343fdf866cebdf3850c
Filip Krunic <fkrunic9@gmail.com>
2023-09-02 14:03:08 -0700

    project setup

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="flex justify-center items-center min-h-screen">
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  </div>
+</template>
+
+<style scoped>
+* {
+  box-sizing: border-box;
+}
+</style>
